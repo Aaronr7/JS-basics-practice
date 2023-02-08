142ecd37a1cfc26ecc4a31e9e47e7ae7d60a608f
Aaron Rafael Santos <94878453+Aaronr7@users.noreply.github.com>
2023-02-08 19:43:20 -0400

    Update: new func removeChild and button event.

--- createElementScript.js
+++ createElementScript.js
@@ -9,6 +9,16 @@ window.onload = function() {
         document.body.appendChild(newDiv);
     };
     
+    const removeElement = (elementParentClass, childElementClass) => {
+        let parent = document.querySeletor(elementParentClass);
+        let child = document.querySelector(childElementClass);
+        parent.removeChild(child);
+    }
+    
     addElement("div", "Hi there! This text bring to the page by method createTextNode");
     addElement("section", "This is a section.");
+    
+    //Button logic
+    let button = document.querySelector(".button");
+    button.addEventListener("click", removeElement(".list-parent",".list"));
     };
