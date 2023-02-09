45c363f856fc4be7703dedd49d9d6d761360a02c
Aaron Rafael Santos <94878453+Aaronr7@users.noreply.github.com>
2023-02-08 21:59:02 -0400

    Update createElementScript.js

--- createElementScript.js
+++ createElementScript.js
@@ -15,9 +15,6 @@ window.onload = function() {
         let throwawayNode = parent.removeChild(child);
     }
     
-    addElement("div", "Hi there! This text bring to the page by method createTextNode");
-    addElement("section", "This is a section.");
-    
     //Button logic
     let removerButton = document.querySelector(".remover-button");
     removerButton.addEventListener("click", () => removeElement(".list-parent",".list"));
