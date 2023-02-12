93a1962b34d993075fddd26640ed848a020529f0
Aaronr7 <tapemusic64@icloud.com>
2023-02-11 20:58:22 -0400

    Prettier format HTML & JS.

--- createElementScript.js
+++ createElementScript.js
@@ -1,68 +1,65 @@
-window.onload = function() {
-    const addElement = (el, content) => {
-        const newDiv = document.createElement(el);
-    
-        const newContent = document.createTextNode(content);
-    
-        newDiv.appendChild(newContent);
-    
-        document.body.appendChild(newDiv);
-    };
+window.onload = function () {
+  const addElement = (el, content) => {
+    const newDiv = document.createElement(el);
 
-    const addUList = () => {
-        const newUl = document.createElement("ul");
-        newUl.classList.add("list");
+    const newContent = document.createTextNode(content);
 
-        let newLi = document.createElement("li");
-        let newContent = document.createTextNode("🍎");
-        newLi.appendChild(newContent);
-        newUl.appendChild(newLi);
+    newDiv.appendChild(newContent);
 
-        newLi = document.createElement("li");
-        newContent = document.createTextNode("🍒");
-        newLi.appendChild(newContent);
-        newUl.appendChild(newLi);
-        
-        newLi = document.createElement("li");
-        newContent = document.createTextNode("🍑");
-        newLi.appendChild(newContent);
-        newUl.appendChild(newLi);
+    document.body.appendChild(newDiv);
+  };
 
-        newLi = document.createElement("li");
-        newContent = document.createTextNode("🍉");
-        newLi.appendChild(newContent);
-        newUl.appendChild(newLi);
+  const addUList = () => {
+    const newUl = document.createElement("ul");
+    newUl.classList.add("list");
 
-        newLi = document.createElement("li");
-        newContent = document.createTextNode("🍇");
-        newLi.appendChild(newContent);
-        newUl.appendChild(newLi);
+    let newLi = document.createElement("li");
+    let newContent = document.createTextNode("🍎");
+    newLi.appendChild(newContent);
+    newUl.appendChild(newLi);
 
-        document.querySelector(".list-parent").appendChild(newUl);
-    };
-    
-    const removeElement = (elementParentClass, childElementClass) => {
-        let parent = document.querySelector(elementParentClass);
-        let child = document.querySelector(childElementClass);
-        let throwawayNode = parent.removeChild(child);
+    newLi = document.createElement("li");
+    newContent = document.createTextNode("🍒");
+    newLi.appendChild(newContent);
+    newUl.appendChild(newLi);
+
+    newLi = document.createElement("li");
+    newContent = document.createTextNode("🍑");
+    newLi.appendChild(newContent);
+    newUl.appendChild(newLi);
+
+    newLi = document.createElement("li");
+    newContent = document.createTextNode("🍉");
+    newLi.appendChild(newContent);
+    newUl.appendChild(newLi);
+
+    newLi = document.createElement("li");
+    newContent = document.createTextNode("🍇");
+    newLi.appendChild(newContent);
+    newUl.appendChild(newLi);
+
+    document.querySelector(".list-parent").appendChild(newUl);
+  };
+
+  const removeElement = (elementParentClass, childElementClass) => {
+    let parent = document.querySelector(elementParentClass);
+    let child = document.querySelector(childElementClass);
+    let throwawayNode = parent.removeChild(child);
+  };
+
+  //Button logic
+  let toggleButton = document.querySelector(".remover-button");
+  toggleButton.addEventListener("click", () => {
+    if (document.querySelector(".list-parent").children.length === 0) {
+      addUList();
+      let nestedUlist = document.querySelector(".list");
+      nestedUlist.childNodes.forEach((liChild) => {
+        liChild.style.backgroundColor = "yellow";
+        liChild.style.padding = "10px";
+        liChild.style.borderRadius = "3px";
+      });
+    } else {
+      removeElement(".list-parent", ".list");
     }
-    
-   
-    //Button logic
-    let toggleButton = document.querySelector(".remover-button");
-    toggleButton.addEventListener("click", () => {
-
-        if (document.querySelector(".list-parent").children.length === 0) {
-            addUList();
-            let nestedUlist = document.querySelector(".list");
-            nestedUlist.childNodes.forEach((liChild) => {
-                liChild.style.backgroundColor = "yellow";
-                liChild.style.padding = "10px";
-                liChild.style.borderRadius = "3px";
-            });
-        } else {
-            removeElement(".list-parent",".list");
-        }
-            
-    });
-    };
+  });
+};
